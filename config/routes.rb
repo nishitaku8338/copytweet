3d0e046d9fbf4cd13e6123d2d388c2129ee12ebd
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-15 17:41:32 +0900

    ツイート投稿ページの実装(newアクションのルーティングを設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'tweets#index'
-  resources :tweets, only: :index
+  root to: 'tweets#index'                  # http://localhost:3000/
+  resources :tweets, only: [:index, :new]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
 end
