2b159603093201054131460413d6e42b8ec27c56
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-15 17:56:56 +0900

    ツイート投稿処理の実装(createアクションのルーティングを設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'                  # http://localhost:3000/
-  resources :tweets, only: [:index, :new]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
+  resources :tweets, only: [:index, :new, :create]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
 end
