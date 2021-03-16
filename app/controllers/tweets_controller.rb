f9975974e081fbb2909a3ece5f386aa42cfc8fae
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-17 00:09:29 +0900

    updateアクションをコントローラーに定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,7 @@ class TweetsController < ApplicationController
   end
 
   def destroy
+    # ビューファイルにインスタンス変数を使用しないので「@」を使わない
     tweet = Tweet.find(params[:id])  # 削除したいツイートをfindメソッドを用いて取得
     tweet.destroy                    # ツイートをdestroyメソッドで削除
   end
@@ -21,8 +22,14 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])  # 編集したいツイートをfindメソッドを用いて取得、form_withのmodelオプション(引数)に使う
   end
 
+  def update
+    # ビューファイルにインスタンス変数を使用しないので「@」を使わない
+    tweet = Tweet.find(params[:id])  # 編集したいツイートをfindメソッドを用いて取得
+    tweet.update(tweet_params)       # tweet_paramsというストロングパラメーターを定義
+  end
+
   private
-  # tweet_paramsというストロングパラメーターを定義し、createメソッドの引数に使用して、tweetsテーブルへ保存
+  # tweet_paramsというストロングパラメーターを定義し、createメソッド・updateメソッドの引数に使用して、tweetsテーブルへ保存
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
