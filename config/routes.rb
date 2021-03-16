2736d0821bba76acc3c5784f9f04dc27e32f6a6b
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-16 23:39:49 +0900

    ツイート編集ページの実装(editアクションのルーティングを設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'                  # http://localhost:3000/
-  resources :tweets, only: [:index, :new, :create, :destroy]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
 end
