0cfd086a9e5d5e48ea9d686e892410aab320e972
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-17 00:21:36 +0900

    ツイート詳細表示機能の実装(showアクションのルーティングを設定)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'                  # http://localhost:3000/
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]  # http://localhost:3000/tweets/new tweetsコントローラーのnewアクションが実行される設定
 end
 
 # updateアクションにはPATCHというHTTPメソッドが使用される
