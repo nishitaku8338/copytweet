658436a425709e3a6cb779a4c708145e0ed5e30c
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-17 00:49:45 +0900

    メソッドによるコード省略(同じコードをメソッドにまとめる)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
-  
+  before_action :set_tweet, only: [:edit, :show]  # set_tweetというメソッドを定義して、同じコードをメソッドにまとめる
+
   def index
     @tweets = Tweet.all  # データベースに保存されているすべてのデータを取得
   end
@@ -19,7 +20,7 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])  # 編集したいツイートをfindメソッドを用いて取得、form_withのmodelオプション(引数)に使う
+    # @tweet = Tweet.find(params[:id])  # 編集したいツイートをfindメソッドを用いて取得、form_withのmodelオプション(引数)に使う
   end
 
   def update
@@ -29,12 +30,17 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])  # ツイート詳細画面に必要な情報を、findメソッドを用いてデータベースから取得
+    # @tweet = Tweet.find(params[:id])  # ツイート詳細画面に必要な情報を、findメソッドを用いてデータベースから取得
   end
 
   private
   # tweet_paramsというストロングパラメーターを定義し、createメソッド・updateメソッドの引数に使用して、tweetsテーブルへ保存
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text)  # require(モデル名)、permit(カラム名)。カラム名はマイグレーションファイルを参照する
+  end
+
+  # set_tweetというメソッドを定義して、editアクション・showアクションの同じコードをまとめる
+  def set_tweet
+    @tweet = Tweet.find(params[:id])  # 単一レコードを取得
   end
 end
