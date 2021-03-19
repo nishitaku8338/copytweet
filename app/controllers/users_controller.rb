f60d137993a04dd041fa4d006d1bd4ecbb56e30e
nishitaku8338 <nishitaku8338@gmail.com>
2021-03-19 13:29:35 +0900

    users_controller.rbを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  
+  # ユーザーマイページのコントローラーshowアクションを定義
+  def show
+    # 現在ログインしているユーザーが持つnicknameカラムの値を取得
+    @nickname = current_user.nickname
+    # 現在ログインしているユーザーのツイート投稿を取得して、インスタンス変数に代入
+    @tweets = current_user.tweets
+  end
+
 end
+
+# showアクションで表示するのは、ログイン中ユーザーのマイページとなるため、
+# 必要な情報は「ニックネーム」と「ログイン中のユーザーのツイート投稿」の2つ
+# それぞれを@nicknameと@tweetsというインスタンス変数に代入する
+# @nicknameはcurrent_userを利用し、現在ログインしているユーザーが持つnicknameカラムの値を取得
+# @tweetsも現在ログインしているユーザーのツイート投稿を取得して、インスタンス変数に代入
