d921b80e3994958c7bf797668a164fc2c19235d6
nishitaku8338 <nishitaku8338@gmail.com>
2021-04-15 10:06:53 +0900

    Userのモデル単体テスト(exampleを整理)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail,passwordとpassword_confirmationが存在すれば登録できる' do
+    end
+    it 'nicknameが6文字以下であれば登録できる' do
+    end
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+    end
     it 'nicknameが空では登録できない' do
       # nicknameが空では登録できないテストコードを記述します
       # nicknameの値が空のインスタンスを生成
@@ -28,6 +34,16 @@ RSpec.describe User, type: :model do
       @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
     end
   end
 end
