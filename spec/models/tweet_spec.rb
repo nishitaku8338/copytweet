f0b6e7567e8eb2525493b532e68cb7d1125faaf7
nishitaku8338 <nishitaku8338@gmail.com>
2021-04-15 10:35:09 +0900

    ツイートを投稿できない場合の記述

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -17,8 +17,14 @@ RSpec.describe Tweet, type: :model do
     end
     context 'ツイートが投稿できない場合' do
       it 'テキストが空では投稿できない' do
+        @tweet.text = ''
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("Text can't be blank")
       end     
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @tweet.user = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('User must exist')
       end
     end
   end
