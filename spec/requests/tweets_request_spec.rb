1a269ff4f71fc467fba796f3acd5100bd81e7370
nishitaku8338 <nishitaku8338@gmail.com>
2021-04-16 17:58:46 +0900

    tweetsコントローラーindexアクションの単体テストコード(exampleを整理)

--- spec/requests/tweets_request_spec.rb
+++ spec/requests/tweets_request_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
+describe TweetsController, type: :request do
 
-RSpec.describe "Tweets", type: :request do
+  before do
+    @tweet = FactoryBot.create(:tweet)
+  end
+
+  describe 'GET #index' do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+    end
+  end
 
 end
+
+# bundle exec rspec spec/requests/tweets_request_spec.rb
+
+# create
+# ActiveRecordのcreateメソッドと同様の意味を持ちます。
+# buildとほぼ同じ働きをしますが、createの場合はテスト用のDBに値が保存されます。
+# 注意すべき点として、1回のテストが実行され、終了する毎にテスト用のDBの内容がロールバックされます。（保存された値がすべて消去されてしまう）
