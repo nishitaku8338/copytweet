87e937a9d07f7d873d1bf1334d2f5fc07480ea39
nishitaku8338 <nishitaku8338@gmail.com>
2021-04-16 18:21:41 +0900

    投稿検索フォームがレスポンスに含まれることを確認しよう投稿検索フォームがレスポンスに含まれることを確認する

--- spec/requests/tweets_request_spec.rb
+++ spec/requests/tweets_request_spec.rb
@@ -19,6 +19,8 @@ describe TweetsController, type: :request do
       expect(response.body).to include(@tweet.image)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+      get root_path
+      expect(response.body).to include('投稿を検索する')
     end
   end
 
