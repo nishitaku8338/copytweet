4ec2407f45c35f73d02e790522eeb16823f890ff
nishitaku8338 <nishitaku8338@gmail.com>
2021-04-29 01:29:45 +0900

    コントローラーを編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,7 +19,16 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)  # tweet_paramsというストロングパラメーターを定義
+    # Tweet.create(tweet_params)  # tweet_paramsというストロングパラメーターを定義
+    @tweet = Tweet.new(tweet_params)
+    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @tweet.valid?
+      @tweet.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
   end
 
   def destroy
